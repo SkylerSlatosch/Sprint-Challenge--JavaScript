42ad40921a64c0adf6a75515b5e159b3c6048472
Skyler <Skyslatosch13@icloud.com>
2019-08-23 10:41:25 -0700

    finished MVP

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,17 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(num1, num2, callback){
+  return callback(num1, num2);
+}
+
+const subtract = (num1, num2) =>{
+  return num1 - num2;
+}
+
+
+console.log(consume(5,4, subtract));
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +25,34 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function consume(num1, num2, callback){
+  return callback(num1, num2);
+}
+
+const add = (num1, num2) =>{
+  return num1 + num2;
+}
+
+const multiply = (num1, num2) =>{
+  return num1 *num2;
+} 
+function greeting(first, last) {
+  return `Hello, ${first} ${last}, nice to meet you!`;
+}
+
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2, 2, add)); // 4
+ console.log(consume(10, 16, multiply)); // 160
+ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
-
-// Explanation: 
+ 
+// Explanation:  Nested functions can use variables that are not explicitly passed as input arguments because they are completed nested within the Parent function. 
 
 
 const external = "I'm outside the function";
